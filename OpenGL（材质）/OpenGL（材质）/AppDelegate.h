6659bdcd30e21c2ca6e94f61e10e4ac973a8bf1a
cy <q2522535@163.com>
2017-09-25 23:20:47 +0800

    材质

---
+++ OpenGL（材质）/OpenGL（材质）/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGL（材质）
+//
+//  Created by cy on 25/09/2017.
+//  Copyright © 2017 com.jiemu.OpenGLTexture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGL（材质）/OpenGL（材质）/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL（材质）
+//
+//  Created by cy on 25/09/2017.
+//  Copyright © 2017 com.jiemu.OpenGLTexture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
